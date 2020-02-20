b88a667044d5d377b8fd88054a0cd74cf7da1fd5
Marc Auberer <marc.auberer@chillibits.com>
2020-02-20 21:45:56 +0100

    Prepared project for first snapshot version

--- src/test/java/com/chillibits/particulatematterapi/ParticulateMatterApiApplicationTests.java
+++ src/test/java/com/chillibits/particulatematterapi/ParticulateMatterApiApplicationTests.java
@@ -4,11 +4,9 @@
 
 package com.chillibits.particulatematterapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ParticulateMatterApiApplicationTests {
-	@Test
-	void contextLoads() {}
+
 }
