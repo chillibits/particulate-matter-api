6b5b7d49449689b702a7f8489d0cac28f0316e5f
Marc Auberer <marc.auberer@chillibits.com>
2020-04-02 01:45:57 +0200

    Update travis configuration for linux

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
-call travis encrypt-file src\main\java\com\chillibits\particulatematterapi\shared\Credentials.java src\main\java\com\chillibits\particulatematterapi\shared\Credentials.java.enc --add --pro
-call travis encrypt-file src\main\resources\application.yml src\main\resources\application.yml.enc --add --pro
-call travis encrypt-file src\main\resources\pmapi.p12 src\main\resources\pmapi.p12.enc --add --pro
+call travis encrypt-file src/main/java/com/chillibits/particulatematterapi/shared/Credentials.java src/main/java/com/chillibits/particulatematterapi/shared/Credentials.java.enc --add --pro
+call travis encrypt-file src/main/resources/application.yml src/main/resources/application.yml.enc --add --pro
+call travis encrypt-file src/main/resources/pmapi.p12 src/main/resources/pmapi.p12.enc --add --pro
