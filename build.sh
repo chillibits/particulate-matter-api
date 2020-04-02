235055673b1fb2bd05b8f08a0ee157204482cb42
Marc Auberer <marc.auberer@chillibits.com>
2020-04-02 14:10:03 +0200

    Try linux shell script for building

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Secret encryption for Travis
+tar cvf secrets.tar src/main/java/com/chillibits/particulatematterapi/shared/Credentials.java src/main/resources/application.yml src/main/resources/pmapi.p12
+travis encrypt-file secrets.tar secrets.tar.enc --add
