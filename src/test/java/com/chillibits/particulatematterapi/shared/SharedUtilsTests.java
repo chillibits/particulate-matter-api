50f84083a0710e3f8efde0808f6dd8642af12e45
Marc Auberer <marc.auberer@chillibits.com>
2020-04-29 13:29:23 +0200

    Add test to SharedUtils.java

--- src/test/java/com/chillibits/particulatematterapi/shared/SharedUtilsTests.java
+++ src/test/java/com/chillibits/particulatematterapi/shared/SharedUtilsTests.java
@@ -4,9 +4,19 @@
 
 package com.chillibits.particulatematterapi.shared;
 
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class SharedUtilsTests {
+    @Test
+    public void testRound() {
+        double result = SharedUtils.round(1.12345, 2);
+        assertEquals(1.12, result);
 
+        result = SharedUtils.round(2.00001, 0);
+        assertEquals(2, result);
+    }
 }
