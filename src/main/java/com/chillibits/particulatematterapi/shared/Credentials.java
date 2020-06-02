068744b8c7ae8eea6e6838bbd49b1abe6da1df07
Marc Auberer <marc.auberer@sap.com>
2020-06-02 23:06:43 +0200

    Add GitHub Packages integration

---
+++ src/main/java/com/chillibits/particulatematterapi/shared/Credentials.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright © Marc Auberer 2019 - 2020. All rights reserved
+ */
+
+package com.chillibits.particulatematterapi.shared;
+
+public class Credentials {
+    public static final String GOOGLE_API_KEY = System.getenv("PMAPI_GOOGLE_API_KEY");
+}
