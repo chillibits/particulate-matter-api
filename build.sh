25d421c4043a66d3f56b0f497b5b05cbc9c8a866
Marc Auberer <marc.auberer@sap.com>
2020-06-06 21:04:01 +0200

    Include ssl certificate in Docker builds

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Secret encryption for Travis
-tar cvf secrets.tar src/main/java/com/chillibits/particulatematterapi/shared/Credentials.java src/main/resources/application.yml src/main/resources/pmapi.p12
-travis encrypt-file secrets.tar secrets.tar.enc --add --pro
+tar cvf secrets.tar src/main/resources/pmapi.p12
 # Secret encryption for GitHub Actions
 gpg --symmetric --cipher-algo AES256 secrets.tar
